f8396460599d6e61351b7f06eaf6a18af3e635cb
ArthiDa <arthida150@gmail.com>
2023-05-26 17:48:55 +0600

    Home 80%

---
+++ src/Pages/Home/Categories.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useMemo, useState } from "react";
+import Foods from "./Foods";
+import Filter from "./Filter";
+
+const Categories = () => {
+  const defaultFoods = [
+    {
+      id: 1,
+      name: "Black Forest - MD",
+      category: "Cakes",
+      Price: "1520",
+      weight: "600gm",
+    },
+    {
+      id: 2,
+      name: "Red Velvet Cake",
+      category: "Cakes",
+      Price: "1550",
+      weight: "500gm",
+    },
+    {
+      id: 3,
+      name: "Belgian Malted Chocolate",
+      category: "Cakes",
+      Price: "1950",
+      weight: "750gm",
+    },
+    {
+      id: 4,
+      name: "Chocolate Fudge Cake",
+      category: "Cakes",
+      Price: "1945",
+      weight: "750gm",
+    },
+    {
+      id: 5,
+      name: "Dark Chocolate Biscuit",
+      category: "Cookies",
+      Price: "1945",
+      weight: "750gm",
+    },
+    {
+      id: 6,
+      name: "Nutty Almond Tiles",
+      category: "Cookies",
+      Price: "1945",
+      weight: "750gm",
+    },
+    {
+      id: 7,
+      name: "Milk Bread",
+      category: "Breads",
+      Price: "1945",
+      weight: "750gm",
+    },
+    {
+      id: 8,
+      name: "Potato Bread",
+      category: "Breads",
+      Price: "1945",
+      weight: "750gm",
+    },
+  ];
+  const [items, setItems] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("All");
+  useEffect(() => {
+    setItems(defaultFoods);
+    setFiltered(defaultFoods);
+  }, []);
+  return (
+    <div className="mb-5">
+      <Filter 
+        items = {items}
+        setFiltered = {setFiltered}
+        activeCategory = {activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
+      <div className="w-4/5 my-8 mx-auto grid lg:grid-cols-4 grid-cols-1 grid-flow-col gap-4">
+        {filtered.slice(0,4).map((food) => (
+          <Foods key={food.id} foods={food}></Foods>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
 import Feature from './Feature';
+import Categories from './Categories';
+import Product from './Product';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Feature></Feature>
+            <Categories></Categories>
+            <Product></Product>
         </div>
     );
 };
